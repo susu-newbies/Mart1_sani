eb1b8daee28c6c21cfd7af010be2b2a217e4c278
susu-newbies <90126909+susu-newbies@users.noreply.github.com>
2021-09-05 18:34:02 +0530

    Update sani_1.0.ino

--- sani_1.0.ino
+++ sani_1.0.ino
@@ -1,55 +1,63 @@
+/*Serial commands are used to debug the program , in the real time run they are are not needed .
+  so the serial commands are commented in the code*/
+
+
 const int IRSensor = 2; // connect ir sensor to arduino pin 2
 const int motor1 = 4 ; // for motor
-int motor2 = 5 ;
-
-
+const int motor2 = 5 ;//  ""
+const int buzzer = 1 ;// For buzzer
+const int led = 2 ;// for led
 
 void setup()
 {
-
   pinMode (IRSensor, INPUT); // sensor pin INPUT
   pinMode (motor1, OUTPUT); // motor pins OUTPUT
   pinMode (motor2,OUTPUT);
-  Serial.begin(9600);
+  pinMode (buzzer,OUTPUT);
+  pinMode (led,OUTPUT);
+  //Serial.begin(9600);
 }
+
 
 void loop()
 {
   int statusSensor = digitalRead (IRSensor);
   //Serial.println(statusSensor);
-
-  if (statusSensor == 1) {
+  if (statusSensor == 1) // 1mean idil 0 mean getting signal
+  {
     digitalWrite(motor1, LOW); // motor relay LOW
     digitalWrite(motor2,LOW);
-    Serial.println("not dedected");
+    //Serial.print(".");
   }
-
   else
   {
-    
-    Serial.println("Dedected");
+  //Serial.println("Dedected");
     motorON();
     checkexit();
   }
 }
 
-void motorON(){
-  
+void motorON()
+ {
   digitalWrite(motor1, HIGH); 
-  digitalWrite(motor2,LOW);// motor relay  High
-  delay(500);
+  digitalWrite(motor2,LOW);// motor on
+  digitalWrite(buzzer,HIGH);
+  digitalWrite(LED,HIGH);
+  delay(300);
   digitalWrite(motor1, LOW); 
-  digitalWrite(motor2,LOW);// motor relay  High
-  
-  }
-void checkexit(){
-  Serial.println("checking");
-  int status = digitalRead (IRSensor);
-   while( status == 0 ){
-    
-    Serial.println("plz take the hand");
+  digitalWrite(motor2,LOW);// motor off
+  digitalWrite(buzzer,LOW);
+  digitalWrite(LED,LOW);
+ }
+
+
+void checkexit()
+ {
+  //Serial.println("checking");
+  int status = digitalRead (IRSensor); 
+  while( status == 0 )
+   {
+    //Serial.println("plz take the hand");
     status = status = digitalRead (IRSensor);
-    
-  }
-  
+   }
   }
