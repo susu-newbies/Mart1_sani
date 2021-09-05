aa7b1baa25499e20c60f0a4b2e9215c69431ff18
susu-newbies <90126909+susu-newbies@users.noreply.github.com>
2021-09-05 15:25:10 +0530

    Update sani_1.0.ino

--- sani_1.0.ino
+++ sani_1.0.ino
@@ -1,31 +1,55 @@
 const int IRSensor = 2; // connect ir sensor to arduino pin 2
-const int motor = 4 ; // for motor 
+const int motor1 = 4 ; // for motor
+int motor2 = 5 ;
 
 
 
-void setup() 
+void setup()
 {
 
   pinMode (IRSensor, INPUT); // sensor pin INPUT
-  pinMode (motor, OUTPUT); // motor pin OUTPUT
+  pinMode (motor1, OUTPUT); // motor pins OUTPUT
+  pinMode (motor2,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
   int statusSensor = digitalRead (IRSensor);
-  
-  Serial.println(statusSensor);
-  
-  if (statusSensor == 1){
-    digitalWrite(motar, LOW); // motor relay LOW
+  //Serial.println(statusSensor);
+
+  if (statusSensor == 1) {
+    digitalWrite(motor1, LOW); // motor relay LOW
+    digitalWrite(motor2,LOW);
     Serial.println("not dedected");
   }
-  
+
   else
   {
-    digitalWrite(motor, HIGH); // motor relay  High
-    Serial.println("Dedected")
+    
+    Serial.println("Dedected");
+    motorON();
+    checkexit();
+  }
+}
+
+void motorON(){
+  
+  digitalWrite(motor1, HIGH); 
+  digitalWrite(motor2,LOW);// motor relay  High
+  delay(500);
+  digitalWrite(motor1, LOW); 
+  digitalWrite(motor2,LOW);// motor relay  High
+  
+  }
+void checkexit(){
+  Serial.println("checking");
+  int status = digitalRead (IRSensor);
+   while( status == 0 ){
+    
+    Serial.println("plz take the hand");
+    status = status = digitalRead (IRSensor);
+    
   }
   
-}
+  }
