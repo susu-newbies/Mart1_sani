ca3efd98c083fe48c0689748b3c1f14a71deb7f5
susu-newbies <90126909+susu-newbies@users.noreply.github.com>
2021-09-06 13:44:16 +0530

    Update sani_1.0.ino

--- sani_1.0.ino
+++ sani_1.0.ino
@@ -2,19 +2,20 @@
   so the serial commands are commented in the code*/
 
 
-const int IRSensor = 2; // connect ir sensor to arduino pin 2
+const int IRSensor = 3; // connect ir sensor to arduino pin 2
 const int motor1 = 4 ; // for motor
 const int motor2 = 5 ;//  ""
-const int buzzer = 1 ;// For buzzer
-const int led = 2 ;// for led
-
+const int buzzer = 2   ;// For buzzer
+const int ledP = 0 ;// for led
+const int ledN = 1 ;
 void setup()
 {
   pinMode (IRSensor, INPUT); // sensor pin INPUT
   pinMode (motor1, OUTPUT); // motor pins OUTPUT
-  pinMode (motor2,OUTPUT);
-  pinMode (buzzer,OUTPUT);
-  pinMode (led,OUTPUT);
+  pinMode (motor2, OUTPUT);
+  pinMode (buzzer, OUTPUT);
+  pinMode (ledP, OUTPUT);
+  pinMode (ledN, OUTPUT);
   //Serial.begin(9600);
 }
 
@@ -26,39 +27,41 @@ void loop()
   if (statusSensor == 1) // 1mean idil 0 mean getting signal
   {
     digitalWrite(motor1, LOW); // motor relay LOW
-    digitalWrite(motor2,LOW);
+    digitalWrite(motor2, LOW);
     //Serial.print(".");
   }
   else
   {
-  //Serial.println("Dedected");
+    //Serial.println("Dedected");
     motorON();
     checkexit();
-    digitalWrite(buzzer,LOW);
-    digitalWrite(LED,LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(ledP, LOW);
+    digitalWrite(ledN, LOW);
   }
 }
 
 void motorON()
- {
-  digitalWrite(motor1, HIGH); 
-  digitalWrite(motor2,LOW);// motor on
-  digitalWrite(buzzer,HIGH);
-  digitalWrite(LED,HIGH);
+{
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW); // motor on
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(ledP, HIGH);
+  digitalWrite(ledN, LOW);
   delay(350);
-  digitalWrite(motor1, LOW); 
-  digitalWrite(motor2,LOW);// motor off
-  
- }
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, LOW); // motor off
+
+}
 
 
-void checkexit()
- {
+void checkexit() // To confirm that the hand is taken and getting ready for nxt triger
+{
   //Serial.println("checking");
-  int status = digitalRead (IRSensor); 
-  while( status == 0 )
-   {
+  int status = digitalRead (IRSensor);
+  while ( status == 0 )
+  {
     //Serial.println("plz take the hand");
-    status = status = digitalRead (IRSensor);
-   }
+    status = digitalRead (IRSensor);
   }
+}
